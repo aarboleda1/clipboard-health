ce8a2086273e70042e72e13d175bd1a5c548a00a
Anton <anton.arboleda@gmail.com>
2017-05-14 08:25:37 -0700

    [update] - pipeline to create table for each entry

--- ingestion/pipeline.py
+++ ingestion/pipeline.py
@@ -17,12 +17,32 @@ and store data in mongo. The fields we're interested in storing are:
 
 Check server/models/Record.js for an example of the schema.
 
-"""
 
+
+"""
 def main():
+    names=['location', 'education', 'department', 'turnover-rate', 'experience', 'orientation-training-length', 'patientNurseRatio', 'salary', 'shiftLength', 'shift', 'specialSkills', 'recommendDeparment', 'date']
     client = MongoClient('mongodb://localhost:27017/')
     db = client['clipboardinterview']
-    df = pd.read_csv(join(dirname(__file__), '../data/projectnurse.csv'))
-
+    df = pd.read_csv(join(dirname(__file__), '../data/projectnurse.csv'), sep=',', delimiter=None, header='infer', names=names)
+    print df
+		## create a model, parse the model and create a document into the DB
+		## read from db and serve it in ag raph 
 if __name__ == "__main__":
     main()
+
+		
+# https://docs.mongodb.com/getting-started/python/insert/
+# how to insert into DB
+# {
+#   education: 'Bachelors',
+#   experience: '4.5', //years
+#   salary: '30', // per hour
+#   department: 'Oncology Nurse',
+#   patientNurseRatio: '4.7', // 1 is always the nurse
+#   location: {
+#     lat: 42.2808,
+#     lng: 83.7430,
+#   },
+# }
+# location:		
