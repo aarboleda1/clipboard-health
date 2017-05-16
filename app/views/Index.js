6d541a800658fddd0448e79dbe55e7bfec6b9025
Anton <anton.arboleda@gmail.com>
2017-05-15 17:27:15 -0700

    [update] - routing

--- app/views/Index.js
+++ app/views/Index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import Main from '../layouts/Main';
+import Nurses from '../layouts/components/Nurses';
+import NavInstance from '../layouts/components/Nav';
+// import MapComponent from '../layouts/components/MapComponent';
+import ScatterPlot from '../layouts/components/ScatterPlot';
 
 const Index = () => (
   <Main>
     <div className="col-sm-12" id="index">
-      <h1> Test </h1>
+      <h1>Nurse Dashboard</h1>
     </div>
+		<ScatterPlot />
+		<Link to={`/map`}>
+			View Nurse Map
+		</Link>	
+		<Link to={`/nurse-list`}>
+			View Nurse List
+		</Link>					
   </Main>
 );
 
